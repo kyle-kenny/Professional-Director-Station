e64081d4ca41d95de4090e9bd33d72d9b3a9f8cf
yunyun-kyle <122270360+kyle-kenny@users.noreply.github.com>
2026-09-09 12:21:39 +0800

    test: capture root React runtime failures

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
 import './styles.css';
 import './editorial.css';
 import './review.css';
@@ -13,6 +14,6 @@ import './director-console.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppErrorBoundary><App /></AppErrorBoundary>
   </StrictMode>,
 );
